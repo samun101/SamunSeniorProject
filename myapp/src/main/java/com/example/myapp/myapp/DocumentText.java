6a858fffd81fd4add981e3d337b404e764432c7d
samundson101 <samundson@westmont.edu>
2021-03-10 19:30:12 -0800

    Cleaning code by removing example functions that are unnecessary

--- myapp/src/main/java/com/example/myapp/myapp/DocumentText.java
+++ myapp/src/main/java/com/example/myapp/myapp/DocumentText.java
@@ -43,126 +43,7 @@ public class DocumentText extends JPanel {
 
     }
 
-    // Draws the image and text bounding box.
-    public void paintComponent(Graphics g) {
-
-        int height = image.getHeight(this);
-        int width = image.getWidth(this);
-
-        Graphics2D g2d = (Graphics2D) g; // Create a Java2D version of g.
-
-        // Draw the image.
-        g2d.drawImage(image, 0, 0, image.getWidth(this) , image.getHeight(this), this);
-
-        // Iterate through blocks and display polygons around lines of detected text.
-        List<Block> blocks = result.getBlocks();
-        for (Block block : blocks) {
-            DisplayBlockInfo(block);
-            if ((block.getBlockType()).equals("LINE")) {
-                ShowPolygon(height, width, block.getGeometry().getPolygon(), g2d);
-                /*
-                  ShowBoundingBox(height, width, block.getGeometry().getBoundingBox(), g2d);
-                 */
-            } else { // its a word, so just show vertical lines.
-                ShowPolygonVerticals(height, width, block.getGeometry().getPolygon(), g2d);
-            }
-        }
-    }
-
-    // Show bounding box at supplied location.
-    private void ShowBoundingBox(int imageHeight, int imageWidth, BoundingBox box, Graphics2D g2d) {
-
-        float left = imageWidth * box.getLeft();
-        float top = imageHeight * box.getTop();
-
-        // Display bounding box.
-        g2d.setColor(new Color(0, 212, 0));
-        g2d.drawRect(Math.round(left), Math.round(top),
-                Math.round(imageWidth * box.getWidth()), Math.round(imageHeight * box.getHeight()));
-
-    }
-
-    // Shows polygon at supplied location
-    private void ShowPolygon(int imageHeight, int imageWidth, List<Point> points, Graphics2D g2d) {
-
-        g2d.setColor(new Color(0, 0, 0));
-        Polygon polygon = new Polygon();
-
-        // Construct polygon and display
-        for (Point point : points) {
-            polygon.addPoint((Math.round(point.getX() * imageWidth)),
-                    Math.round(point.getY() * imageHeight));
-        }
-        g2d.drawPolygon(polygon);
-    }
-
-    // Draws only the vertical lines in the supplied polygon.
-    private void ShowPolygonVerticals(int imageHeight, int imageWidth, List<Point> points, Graphics2D g2d) {
-
-        g2d.setColor(new Color(0, 212, 0));
-        Object[] parry = points.toArray();
-        g2d.setStroke(new BasicStroke(2));
-
-        g2d.drawLine(Math.round(((Point) parry[0]).getX() * imageWidth),
-                Math.round(((Point) parry[0]).getY() * imageHeight), Math.round(((Point) parry[3]).getX() * imageWidth),
-                Math.round(((Point) parry[3]).getY() * imageHeight));
-
-        g2d.setColor(new Color(255, 0, 0));
-        g2d.drawLine(Math.round(((Point) parry[1]).getX() * imageWidth),
-                Math.round(((Point) parry[1]).getY() * imageHeight), Math.round(((Point) parry[2]).getX() * imageWidth),
-                Math.round(((Point) parry[2]).getY() * imageHeight));
-
-    }
-    //Displays information from a block returned by text detection and text analysis
-    private void DisplayBlockInfo(Block block) {
-        System.out.println("Block Id : " + block.getId());
-        if (block.getText()!=null)
-            System.out.println("    Detected text: " + block.getText());
-        System.out.println("    Type: " + block.getBlockType());
-        
-        if (block.getBlockType().equals("PAGE") !=true) {
-            System.out.println("    Confidence: " + block.getConfidence().toString());
-        }
-        if(block.getBlockType().equals("CELL"))
-        {
-            System.out.println("    Cell information:");
-            System.out.println("        Column: " + block.getColumnIndex());
-            System.out.println("        Row: " + block.getRowIndex());
-            System.out.println("        Column span: " + block.getColumnSpan());
-            System.out.println("        Row span: " + block.getRowSpan());
-
-        }
-        
-        System.out.println("    Relationships");
-        List<Relationship> relationships=block.getRelationships();
-        if(relationships!=null) {
-            for (Relationship relationship : relationships) {
-                System.out.println("        Type: " + relationship.getType());
-                System.out.println("        IDs: " + relationship.getIds().toString());
-            }
-        } else {
-            System.out.println("        No related Blocks");
-        }
-
-        System.out.println("    Geometry");
-        System.out.println("        Bounding Box: " + block.getGeometry().getBoundingBox().toString());
-        System.out.println("        Polygon: " + block.getGeometry().getPolygon().toString());
-        
-        List<String> entityTypes = block.getEntityTypes();
-        
-        System.out.println("    Entity Types");
-        if(entityTypes!=null) {
-            for (String entityType : entityTypes) {
-                System.out.println("        Entity Type: " + entityType);
-            }
-        } else {
-            System.out.println("        No entity type");
-        }
-        if(block.getPage()!=null)
-            System.out.println("    Page: " + block.getPage());            
-        System.out.println();
-    }
-    
+   
     public static void saveString(String str) throws Exception{ //small function to help save strings and make sure I'm getting the content correctly
     	try {
         	File myObj = new File("C:\\Users\\Sam\\Documents\\toString.txt");
@@ -193,16 +74,6 @@ public class DocumentText extends JPanel {
         String bucket = "samunbucket";
 
         
-        AmazonS3 s3client = AmazonS3ClientBuilder.standard()
-                .withEndpointConfiguration( 
-                        new EndpointConfiguration("https://s3.amazonaws.com","us-east-2"))
-                .build();
-        
-               
-        // Get the document from S3
-        com.amazonaws.services.s3.model.S3Object s3object = s3client.getObject(bucket, document);
-        S3ObjectInputStream inputStream = s3object.getObjectContent();
-        BufferedImage image = ImageIO.read(inputStream);
 
         // Call DetectDocumentText
         EndpointConfiguration endpoint = new EndpointConfiguration(
@@ -215,16 +86,8 @@ public class DocumentText extends JPanel {
             .withDocument(new Document().withS3Object(new S3Object().withName(document).withBucket(bucket)));
 
         DetectDocumentTextResult result = client.detectDocumentText(request);
-/*        // Create frame and panel.
-        JFrame frame = new JFrame("RotateImage");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        DocumentText panel = new DocumentText(result, image);
-        panel.setPreferredSize(new Dimension(image.getWidth() , image.getHeight() ));
-        frame.setContentPane(panel);
-        frame.pack();
-        frame.setVisible(true);
-        //System.out.println(result.toString());
- */       
+        
+        //reading text and copying it to files
         String res = "";
         for(Block block:result.getBlocks()) {
         	if(block.getBlockType().equals("LINE")) {
