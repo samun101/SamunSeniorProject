38402e0e37c6e0303ea24eae737358c6c5ced797
samundson101 <samundson@westmont.edu>
2021-03-10 19:30:28 -0800

    Fixing last commit

--- myapp/src/main/java/com/example/myapp/myapp/DocumentText.java
+++ myapp/src/main/java/com/example/myapp/myapp/DocumentText.java
@@ -5,12 +5,9 @@ package com.example.myapp.myapp;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.util.List;
-import javax.imageio.ImageIO;
-
 import java.io.File;  // Import the File class
 import java.io.IOException;  // Import the IOException class to handle errors
 import java.io.FileWriter;   // Import the FileWriter class
-
 import javax.swing.*;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
