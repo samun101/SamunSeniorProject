ca0f69c9051664c4cf398c54c0c25929b5b17f74
samundson101 <samundson@westmont.edu>
2021-03-10 19:23:49 -0800

    Update 2 (3/10/21)
    Changing to work with (relatively) simpler DetectDocumentText and
    turning blocks into workible strings

--- myapp/src/main/java/com/example/myapp/myapp/DocumentText.java
+++ myapp/src/main/java/com/example/myapp/myapp/DocumentText.java
@@ -1,38 +1,43 @@
-//Loads document from S3 bucket. Displays the document and polygon around detected lines of text.
+//Calls DetectDocumentText.
+//Loads document from S3 bucket. Displays the document and bounding boxes around detected lines/words of text.
 package com.example.myapp.myapp;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.util.List;
 import javax.imageio.ImageIO;
+
+import java.io.File;  // Import the File class
+import java.io.IOException;  // Import the IOException class to handle errors
+import java.io.FileWriter;   // Import the FileWriter class
+
 import javax.swing.*;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.S3ObjectInputStream;
+import com.amazonaws.client.builder.AwsClientBuilder.EndpointConfiguration;
 import com.amazonaws.services.textract.AmazonTextract;
 import com.amazonaws.services.textract.AmazonTextractClientBuilder;
-import com.amazonaws.services.textract.model.AnalyzeDocumentRequest;
-import com.amazonaws.services.textract.model.AnalyzeDocumentResult;
 import com.amazonaws.services.textract.model.Block;
 import com.amazonaws.services.textract.model.BoundingBox;
+import com.amazonaws.services.textract.model.DetectDocumentTextRequest;
+import com.amazonaws.services.textract.model.DetectDocumentTextResult;
 import com.amazonaws.services.textract.model.Document;
 import com.amazonaws.services.textract.model.S3Object;
 import com.amazonaws.services.textract.model.Point;
 import com.amazonaws.services.textract.model.Relationship;
-import com.amazonaws.client.builder.AwsClientBuilder.EndpointConfiguration;
 
-public class AnalyzeDocument extends JPanel {
+
+public class DocumentText extends JPanel {
 
     private static final long serialVersionUID = 1L;
 
     BufferedImage image;
+    DetectDocumentTextResult result;
 
-    AnalyzeDocumentResult result;
-
-    public AnalyzeDocument(AnalyzeDocumentResult documentResult, BufferedImage bufImage) throws Exception {
+    public DocumentText(DetectDocumentTextResult documentResult, BufferedImage bufImage) throws Exception {
         super();
-
-
+        
         result = documentResult; // Results of text detection.
         image = bufImage; // The image containing the document.
 
@@ -47,49 +52,31 @@ public class AnalyzeDocument extends JPanel {
         Graphics2D g2d = (Graphics2D) g; // Create a Java2D version of g.
 
         // Draw the image.
-        g2d.drawImage(image, 0, 0, image.getWidth(this), image.getHeight(this), this);
+        g2d.drawImage(image, 0, 0, image.getWidth(this) , image.getHeight(this), this);
 
-        // Iterate through blocks and display bounding boxes around everything.
-
+        // Iterate through blocks and display polygons around lines of detected text.
         List<Block> blocks = result.getBlocks();
         for (Block block : blocks) {
             DisplayBlockInfo(block);
-       /*     switch(block.getBlockType()) {
-            
-            case "KEY_VALUE_SET":
-                if (block.getEntityTypes().contains("KEY")){
-                    ShowBoundingBox(height, width, block.getGeometry().getBoundingBox(), g2d, new Color(255,0,0));
-                }
-                else {  //VALUE
-                    ShowBoundingBox(height, width, block.getGeometry().getBoundingBox(), g2d, new Color(0,255,0));
-                }
-                break;
-            case "TABLE":
-                ShowBoundingBox(height, width, block.getGeometry().getBoundingBox(), g2d, new Color(0,0,255));
-                break;
-            case "CELL":
-                ShowBoundingBox(height, width, block.getGeometry().getBoundingBox(), g2d, new Color(255,255,0));
-                break;
-            default:*/
-                //PAGE, LINE & WORD
-                ShowBoundingBox(height, width, block.getGeometry().getBoundingBox(), g2d, new Color(200,200,0));
-            //}
+            if ((block.getBlockType()).equals("LINE")) {
+                ShowPolygon(height, width, block.getGeometry().getPolygon(), g2d);
+                /*
+                  ShowBoundingBox(height, width, block.getGeometry().getBoundingBox(), g2d);
+                 */
+            } else { // its a word, so just show vertical lines.
+                ShowPolygonVerticals(height, width, block.getGeometry().getPolygon(), g2d);
+            }
         }
-
-         // uncomment to show polygon around all blocks
-         //ShowPolygon(height,width,block.getGeometry().getPolygon(),g2d);
-      
-      
     }
 
     // Show bounding box at supplied location.
-    private void ShowBoundingBox(int imageHeight, int imageWidth, BoundingBox box, Graphics2D g2d, Color color) {
+    private void ShowBoundingBox(int imageHeight, int imageWidth, BoundingBox box, Graphics2D g2d) {
 
         float left = imageWidth * box.getLeft();
         float top = imageHeight * box.getTop();
 
         // Display bounding box.
-        g2d.setColor(color);
+        g2d.setColor(new Color(0, 212, 0));
         g2d.drawRect(Math.round(left), Math.round(top),
                 Math.round(imageWidth * box.getWidth()), Math.round(imageHeight * box.getHeight()));
 
@@ -107,6 +94,24 @@ public class AnalyzeDocument extends JPanel {
                     Math.round(point.getY() * imageHeight));
         }
         g2d.drawPolygon(polygon);
+    }
+
+    // Draws only the vertical lines in the supplied polygon.
+    private void ShowPolygonVerticals(int imageHeight, int imageWidth, List<Point> points, Graphics2D g2d) {
+
+        g2d.setColor(new Color(0, 212, 0));
+        Object[] parry = points.toArray();
+        g2d.setStroke(new BasicStroke(2));
+
+        g2d.drawLine(Math.round(((Point) parry[0]).getX() * imageWidth),
+                Math.round(((Point) parry[0]).getY() * imageHeight), Math.round(((Point) parry[3]).getX() * imageWidth),
+                Math.round(((Point) parry[3]).getY() * imageHeight));
+
+        g2d.setColor(new Color(255, 0, 0));
+        g2d.drawLine(Math.round(((Point) parry[1]).getX() * imageWidth),
+                Math.round(((Point) parry[1]).getY() * imageHeight), Math.round(((Point) parry[2]).getX() * imageWidth),
+                Math.round(((Point) parry[2]).getY() * imageHeight));
+
     }
     //Displays information from a block returned by text detection and text analysis
     private void DisplayBlockInfo(Block block) {
@@ -157,15 +162,37 @@ public class AnalyzeDocument extends JPanel {
             System.out.println("    Page: " + block.getPage());            
         System.out.println();
     }
-    public static void findText(AnalyzeDocumentResult documentResult) throws Exception{
-
+    
+    public static void saveString(String str) throws Exception{ //small function to help save strings and make sure I'm getting the content correctly
+    	try {
+        	File myObj = new File("C:\\Users\\Sam\\Documents\\toString.txt");
+            if (myObj.createNewFile()) {
+              System.out.println("File created: " + myObj.getName());
+            } else {
+              System.out.println("File already exists.");
+            }
+          } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+          }
+        try {
+            FileWriter myWriter = new FileWriter("C:\\Users\\Sam\\Documents\\toString.txt");
+            myWriter.write(str);
+            myWriter.close();
+            System.out.println("Successfully wrote to the file.");
+          } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+          }
     }
+    
     public static void main(String arg[]) throws Exception {
-
+        
         // The S3 bucket and document
         String document = "Test.PNG";
         String bucket = "samunbucket";
 
+        
         AmazonS3 s3client = AmazonS3ClientBuilder.standard()
                 .withEndpointConfiguration( 
                         new EndpointConfiguration("https://s3.amazonaws.com","us-east-2"))
@@ -177,30 +204,35 @@ public class AnalyzeDocument extends JPanel {
         S3ObjectInputStream inputStream = s3object.getObjectContent();
         BufferedImage image = ImageIO.read(inputStream);
 
-        // Call AnalyzeDocument 
+        // Call DetectDocumentText
         EndpointConfiguration endpoint = new EndpointConfiguration(
                 "https://textract.us-east-2.amazonaws.com", "us-east-2");
         AmazonTextract client = AmazonTextractClientBuilder.standard()
                 .withEndpointConfiguration(endpoint).build();
 
-                
-        AnalyzeDocumentRequest request = new AnalyzeDocumentRequest()
-                .withFeatureTypes("TABLES","FORMS")
-                 .withDocument(new Document().
-                        withS3Object(new S3Object().withName(document).withBucket(bucket)));
 
+        DetectDocumentTextRequest request = new DetectDocumentTextRequest()
+            .withDocument(new Document().withS3Object(new S3Object().withName(document).withBucket(bucket)));
 
-        AnalyzeDocumentResult result = client.analyzeDocument(request);
-
-        // Create frame and panel.
+        DetectDocumentTextResult result = client.detectDocumentText(request);
+/*        // Create frame and panel.
         JFrame frame = new JFrame("RotateImage");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        AnalyzeDocument panel = new AnalyzeDocument(result, image);
-        panel.setPreferredSize(new Dimension(image.getWidth(), image.getHeight()));
+        DocumentText panel = new DocumentText(result, image);
+        panel.setPreferredSize(new Dimension(image.getWidth() , image.getHeight() ));
         frame.setContentPane(panel);
         frame.pack();
         frame.setVisible(true);
+        //System.out.println(result.toString());
+ */       
+        String res = "";
+        for(Block block:result.getBlocks()) {
+        	if(block.getBlockType().equals("LINE")) {
+        		res = res + String.valueOf(block.getGeometry().getBoundingBox().getLeft())+" "+ block.getText() + "\n";
+        	}
+        }
+        System.out.println(res);
+        saveString(res);
         
-        System.out.println(result);
     }
 }
