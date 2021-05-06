f9ddbf5948d67af52998742106fcdb5537fc3a55
samundson101 <samundson@westmont.edu>
2021-05-06 16:32:31 -0700

    getting to final stage of project, ready for presentation

--- myapp/src/main/java/com/example/myapp/myapp/DocumentProcessor.java
+++ myapp/src/main/java/com/example/myapp/myapp/DocumentProcessor.java
@@ -8,6 +8,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.SdkClientException;
 import com.amazonaws.auth.policy.Condition;
 import com.amazonaws.auth.policy.Policy;
 import com.amazonaws.auth.policy.Principal;
@@ -17,6 +19,10 @@ import com.amazonaws.auth.policy.Statement.Effect;
 import com.amazonaws.auth.policy.actions.SQSActions;
 import com.amazonaws.client.builder.AwsClientBuilder.EndpointConfiguration;
 import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.amazonaws.services.sns.AmazonSNS;
 import com.amazonaws.services.sns.AmazonSNSClientBuilder;
 import com.amazonaws.services.sns.model.CreateTopicRequest;
@@ -32,6 +38,7 @@ import com.amazonaws.services.textract.AmazonTextractClientBuilder;
 import com.amazonaws.services.textract.model.Block;
 import com.amazonaws.services.textract.model.DocumentLocation;
 import com.amazonaws.services.textract.model.DocumentMetadata;
+import com.amazonaws.services.textract.model.Geometry;
 import com.amazonaws.services.textract.model.GetDocumentTextDetectionRequest;
 import com.amazonaws.services.textract.model.GetDocumentTextDetectionResult;
 import com.amazonaws.services.textract.model.NotificationChannel;
@@ -62,14 +69,20 @@ public class DocumentProcessor {
 
     public static void main(String[] args) throws Exception {
         
-        String document = "volo2IMG.pdf";
+    	//String document = "Volo2IMGsmall.pdf";
+        String document = "mtf2IMGsmall.pdf";
         String bucket = "samunbucket";
         String roleArn="arn:aws:iam::534755077870:role/TextractRole";
         Regions clientRegion = Regions.US_EAST_2;
+        //String fileName ="C:/Users/Sam/Downloads/Test.PNG";
+        
+        //uploader(clientRegion,bucket,document,fileName);
         
         sns = AmazonSNSClientBuilder.standard().withRegion(clientRegion).build();
         sqs= AmazonSQSClientBuilder.standard().withRegion(clientRegion).build();
         
+
+
         // Build Textract Client
         EndpointConfiguration endpoint = new EndpointConfiguration(
                 "https://textract.us-east-2.amazonaws.com", "us-east-2");
@@ -241,7 +254,7 @@ public class DocumentProcessor {
           }
         try {
             FileWriter myWriter = new FileWriter("C:\\Users\\Sam\\Documents\\toString.txt");
-            myWriter.write(str);
+            myWriter.append(str);
             myWriter.close();
             System.out.println("Successfully wrote to the file.");
           } catch (IOException e) {
@@ -275,9 +288,36 @@ public class DocumentProcessor {
         String paginationToken=null;
         GetDocumentTextDetectionResult response=null;
         Boolean finished=false;
+        
+        Integer pageL = 0;
+        Integer pageR =0;
+        
         String ret = "";
-        String temp = "";
-        Integer count = 1;
+        
+        String tempL = "";
+        String tempR = "";
+        
+        Integer Lcount = 1;
+        Integer Rcount = 1;
+        
+        String Lbuf = "";
+        String Rbuf = "";
+        
+        Boolean side = false;
+        
+        Boolean Lact = false;
+        Boolean Ract = false;
+        
+        Float Llast = 0.0f;
+        Float Ltlast;
+        Float Rlast = 0.0f;
+        Float Rtlast;
+    
+        Integer Cpage;
+        
+        String Lback = "";
+        Boolean Llen = false;
+        
         while (finished==false)
         {
             GetDocumentTextDetectionRequest documentTextDetectionRequest= new GetDocumentTextDetectionRequest()
@@ -285,34 +325,121 @@ public class DocumentProcessor {
                     .withMaxResults(maxResults)
                     .withNextToken(paginationToken);
             response = textract.getDocumentTextDetection(documentTextDetectionRequest);
-            DocumentMetadata documentMetaData=response.getDocumentMetadata();
-
-            System.out.println("Pages: " + documentMetaData.getPages().toString());
+            
             
             //Show blocks information
             List<Block> blocks= response.getBlocks();
             for (Block block : blocks) {
             	//Here is where data processing goes
             	if(block.getBlockType().equals("LINE")) {
-            		if(block.getText().equals(block.getText().toUpperCase())) {
-            			temp = DisplayBlockInfo(block);
-            			count++;
-            		}
-            		if(count >4) {
-            			count =0;
-            		}
-            		if(count >0 ) {
-            			if(block.getText().contains("Medium")) {
-            				count = 0;
-            				ret = ret+temp;
-            				temp = "";
-            			}
-            			else {
-            				count++;
-            			}
-            		}            		
-            	}        	
-                //ret = ret +DisplayBlockInfo(block);
+            		//System.out.println("Pages: " + block.getPage());
+            		side = checkSide(block.getGeometry());
+            		Cpage = block.getPage();
+            		
+	            	if(!side){  //Processing the left side of the page
+	            		Ltlast = Llast;
+	            		Llast = block.getGeometry().getBoundingBox().getTop();
+	            		tempL = DisplayBlockInfo(block);
+	            		if(!Lact) {//finding the start of a statblock
+		            		if(checkLegal(block.getText())) {
+		            			Lbuf = tempL;
+		            			Lcount=1;
+		            			pageL=block.getPage();
+		            		}
+		            		if(Lcount >3) {
+		            			Lcount =0;
+		            			Lbuf="";
+		            		}
+		            		if(Lcount >0) {
+		            			if(checkSizes(tempL)&& !Lbuf.equals(tempL)) {
+		            				Lcount = 0;
+		            				Lact = true;
+		            				Lbuf = Lbuf + tempL;
+		            				tempL = "";
+		            			}
+		            			else {
+		            				Lcount++;
+		            			}
+		            		}
+	            		}
+	            		if(Lact) {//saving the statblock
+	            			Llen = checkActions(tempL) || Llen;
+	            			//System.out.println(Llen);
+	            			if(pageL == Cpage && Llast-Ltlast < 0.035){//checking for end of statblock
+	            				Lbuf = Lbuf+tempL;
+	            			}
+	            			else { 
+	            				if(!Llen) {
+		            				ret = ret+Lbuf+Lback +"\n";
+		            				Lact=false;
+		            				Llen=false;
+		            				Lback="";
+		            				Lbuf="";
+		            			}
+		            			else{
+		            				ret = ret+Lbuf +"\n";
+		            				Lact=false;
+		            				Llen=false;
+		            				Lbuf="";
+		            				Lback ="";
+		            			}
+	            			}
+	            			
+	            		}
+	            	}
+	            	if(side){  //Processing the right side of the page
+	            		Rtlast = Rlast;
+	            		Rlast = block.getGeometry().getBoundingBox().getTop();
+	            		tempR = DisplayBlockInfo(block);
+	            		if(!Ract) {
+			            		if(tempR.equals(tempR.toUpperCase()) && !checkActions(tempR)) {
+			            			//System.out.println(tempR);
+			            			Rbuf = tempR;
+			            			Rcount=1;
+			            			pageR=block.getPage();
+			            		}
+			            		
+			            		if(Rcount >4) {
+			            			Rcount =0;
+			            			Rbuf="";
+			            		}
+			            		if(Rcount >0) {
+			            			if(checkSizes(tempR)) {
+			            				Rcount = 0;
+			            				Ract = true;
+			            				Rbuf = Rbuf + tempR;
+			            				tempR = "";
+			            				Lback="";
+			            			}
+			            			else {
+			            				Rcount++;
+			            			}
+			            		}
+			            		if(Lact || Lcount>0) {
+		            				Lback = Lback+tempR;
+		            			}
+		            			else {
+		            				Lback="";
+		            			}
+	            			
+	            			}
+	            		if(Ract) {
+	            			if(pageR == Cpage && Rlast-Rtlast<0.035){
+	            				Rbuf = Rbuf+tempR;
+	            			}
+	            			else if(checkActions(tempR)) {
+	            				Rbuf = Rbuf+tempR;
+	            			}
+	            			else {
+	            				Ract=false;
+	            				ret = ret+Rbuf+"\n";
+	            				Rbuf="";
+	            			}
+	            		}
+	            	}
+                }        	
+                
+
             }
             paginationToken=response.getNextToken();
             if (paginationToken==null)
@@ -321,23 +448,112 @@ public class DocumentProcessor {
         }
         return ret;
     }
-
+    //get whether text is on the left or right side of the page
+    private static boolean checkSide(Geometry vals) {
+    	if(vals.getBoundingBox().getLeft() >= 0.45){
+    		return true;
+    	}
+    	else {
+    		return false;
+    	}
+    }
+    
+    //check if string contains item from a list
+    public static boolean checkSizes(String inputStr)
+    {
+        Integer count = 0;
+        char ch[]=new char[inputStr.length()];
+    	String[] sizes = {"Small","Medium","Large","Huge","Gargantuan","Tiny", "chaotic ","neutral","lawful ","good","evil","chactic"}; 
+        
+        	for(int c=0;c<inputStr.length();c++) {
+        		ch[c]= inputStr.charAt(c);  
+                if( ((c>0)&&(ch[c]!=' ')&&(ch[c-1]==' ')) || ((ch[0]!=' ')&&(c==0)) )  
+                    count++; 
+            }
+        	if(count > 8|| count<3) return false;
+        	
+        	for(int i =0; i < sizes.length; i++)
+            {
+                if(inputStr.contains(sizes[i]))
+                {
+                    return true;
+                }
+            }
+            return false;   
+    }
+    
+  //check if string contains item from a list
+    public static boolean checkActions(String inputStr)
+    {
+    	String[] actions = {"ACTIONS","LEGENDARY ACTIONS", "REACTIONS","ACTI0NS"}; 
+    	String[] avoid = {"LAIR","REGIONAL"};
+    	for(int i =0; i < avoid.length; i++)
+        {
+            if(inputStr.contains(avoid[i]))
+            {
+                return false;
+            }
+        }
+    	for(int i =0; i < actions.length; i++)
+        {
+            if(inputStr.contains(actions[i]))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
     //Displays Block information for text detection and text analysis
     private static String DisplayBlockInfo(Block block) {
     	String ret = "";
     	if(block.getBlockType().equals("LINE")) {
-	        //System.out.println("Block Id : " + block.getId());
 	        if (block.getText()!=null) {
-	            //System.out.println("\tDetected text: " + block.getText());
-	            ret = ret + block.getText() +"\n";
+	            ret = block.getText() +"\n";
 	        }
 	        if(block.getPage()!=null) {
-	            //System.out.println("\tPage: " + block.getPage());            
-	        //System.out.println();
 	    	}
     	}
     	return ret;
     	
     }
-    	
+    
+    
+    private static boolean checkLegal(String inputStr) {
+     
+    	try {
+        	Double.parseDouble(inputStr);
+        } catch (NumberFormatException e) {
+        	return inputStr.equals(inputStr.toUpperCase());
+        }
+    	return false;
+    }
+    
+    private static void uploader(Regions clientRegion, String bucketName,String fileObjKeyName,String fileName) {
+	    try {
+	        //This code expects that you have AWS credentials set up per:
+	        // https://docs.aws.amazon.com/sdk-for-java/v1/developer-guide/setup-credentials.html
+	        AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
+	                .withRegion(clientRegion)
+	                .build();
+	
+	        // Upload a text string as a new object.
+	        //s3Client.putObject(bucketName, stringObjKeyName, "Uploaded String Object");
+	
+	        // Upload a file as a new object with ContentType and title specified.
+	        PutObjectRequest request = new PutObjectRequest(bucketName, fileObjKeyName, new File(fileName));
+	        ObjectMetadata metadata = new ObjectMetadata();
+	        metadata.setContentType("plain/text");
+	        metadata.addUserMetadata("title", "someTitle");
+	        request.setMetadata(metadata);
+	        s3Client.putObject(request);
+	    } catch (AmazonServiceException e) {
+	        // The call was transmitted successfully, but Amazon S3 couldn't process 
+	        // it, so it returned an error response.
+	        e.printStackTrace();
+	    } catch (SdkClientException e) {
+	        // Amazon S3 couldn't be contacted for a response, or the client
+	        // couldn't parse the response from Amazon S3.
+	        e.printStackTrace();
+	    }
+    }
 }
